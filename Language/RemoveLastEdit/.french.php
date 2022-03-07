0134413aa595a0474c192950d450628b267c692f
Diego Andrés <diegoandres_cortes@outlook.com>
2022-03-06 20:06:36 -0600

    add french translation
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Language/RemoveLastEdit/.french.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package Remove Last Edit
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+// Remove "Last edit" mod
+$txt['remove_edit_sign'] = 'Retirer ce message';
+$txt['remove_last_edited_error_1'] = 'Données erronées dans la fonction rlem_do, Subs-RemoveLastEditMod.php';
+$txt['remove_last_edited_error_2'] = 'Vous ne pouvez pas effectuer cette action.';
+$txt['remove_last_edited_error_3'] = 'Action non valide.';
+$txt['permissionname_rlem_do'] = 'Retirer \'Dernière édition par ...\' du message';
+$txt['permissionname_rlem_do_own'] = 'Retirer \'Dernière édition par ...\' de tous vos messages';
+$txt['permissionname_rlem_do_any'] = 'Retirer \'Dernière édition par ...\' de tous les messages';
+$txt['permissiongroup_simple_rlem_simple'] = 'Retirer les permissions du mod "Last Edit"';
+$txt['permissiongroup_rlem_classic'] = 'Retirer les permissions du mod "Last Edit"';
